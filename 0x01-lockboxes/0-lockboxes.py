aefdd545661878119b223ea0ac44a302f66b0069
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-01 17:22:01 +0000

    Task 0 - solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ Determines if all the boxes can be opened. Each
+        box is numbered sequentially from 0 to n - 1 and
+        each box may contain keys to the other boxes
+    """
+    openedBoxes = set()
+    boxesLength = len(boxes)
+    box = [0]
+
+    while box:
+        key = box.pop()
+        openedBoxes.add(key)
+
+        for value in boxes[key]:
+            if value < boxesLength and value not in openedBoxes:
+                box.append(value)
+
+    return len(openedBoxes) == len(boxes)
