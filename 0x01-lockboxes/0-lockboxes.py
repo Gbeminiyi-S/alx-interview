01842762793b624c671c01d765aa101cb5111f40
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-01 17:29:14 +0000

    update to check if entry is a list of lists

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ This Module contains a function `canUnlockAll` """
 
+
 def canUnlockAll(boxes):
     """ Determines if all the boxes can be opened. Each
         box is numbered sequentially from 0 to n - 1 and
         each box may contain keys to the other boxes
     """
+
+    if type(boxes) != list:
+        return False
+
     openedBoxes = set()
     boxesLength = len(boxes)
     box = [0]
@@ -13,6 +18,9 @@ def canUnlockAll(boxes):
     while box:
         key = box.pop()
         openedBoxes.add(key)
+
+        if type(boxes[key]) != list:
+            return False
 
         for value in boxes[key]:
             if value < boxesLength and value not in openedBoxes:
