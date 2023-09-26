58d20c5e354e48112225b7bf6f5e99bb3024deed
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-09-26 19:14:34 +0000

    adding module documentation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""This module defines the function `island_perimeter`"""
+
 
 def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
     size = len(grid)
     perimeter = 0
 
     for i in range(size):
-        for j in range (size):
+        for j in range(size):
             if (0 < i < size - 1) and (0 < j < size - 1):
                 if grid[i - 1][j]:
                     perimeter += 1
