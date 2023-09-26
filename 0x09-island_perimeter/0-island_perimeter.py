ccbfd43eac243631fee6aae685543f410b63dfc9
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-09-26 21:57:50 +0000

    new solutiom

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,24 +4,20 @@
 
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
-    size = len(grid)
+    col_size = len(grid[0])
+    row_size = len(grid)
     perimeter = 0
 
-    for i in range(size):
-        for j in range(size):
+    for i in range(row_size):
+        for j in range(col_size):
             if grid[i][j]:
-                #if (0 < i <= size - 1) and (0 <= j < size - 1):
-                    #print(f"{i} && {j}\n")
-                    if grid[i - 1][j] == 0:
-                        #print(i - 1, j)
+                if (0 < i < row_size) or (0 < j < col_size):
+                    if j - 1 < 0 or grid[i][j - 1] == 0:
                         perimeter += 1
-                    if grid[i][j - 1] == 0:
-                        #print(i, j - 1)
+                    if j + 1 >= col_size or grid[i][j + 1] == 0:
                         perimeter += 1
-                    if grid[i][j + 1] == 0:
-                        #print(i, j + 1)
+                    if i + 1 >= row_size or grid[i + 1][j] == 0:
                         perimeter += 1
-                    if grid[i + 1][j] == 0:
-                        #print(i + 1, j)
+                    if i - 1 < 0 or grid[i - 1][j] == 0:
                         perimeter += 1
     return perimeter
